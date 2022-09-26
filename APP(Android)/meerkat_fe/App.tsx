72e11a5b75b50c93561a461bca5a3059d0dab0f9
Seho Lee <31482043+seho0808@users.noreply.github.com>
2022-09-26 07:05:00 +0000

    MK-22 feat: custom fonts applied

--- APP(Android)/meerkat_fe/App.tsx
+++ APP(Android)/meerkat_fe/App.tsx
@@ -1,6 +1,20 @@
-import ChatRoomList from './pages/ChatRoomList';
+import { useCallback, useEffect, useState } from "react";
+import { useFonts } from "expo-font";
+import ChatRoomList from "./pages/ChatRoomList";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "noto-black": require("./assets/fonts/NotoSansKR-Black.otf"),
+    "noto-bold": require("./assets/fonts/NotoSansKR-Bold.otf"),
+    "noto-light": require("./assets/fonts/NotoSansKR-Light.otf"),
+    "noto-med": require("./assets/fonts/NotoSansKR-Medium.otf"),
+    "noto-reg": require("./assets/fonts/NotoSansKR-Regular.otf"),
+    "noto-thin": require("./assets/fonts/NotoSansKR-Thin.otf"),
+  });
 
-  return <ChatRoomList/>
+  return (
+    <>
+      {fontsLoaded && <ChatRoomList />}
+    </>
+  );
 }
--- APP(Android)/meerkat_fe/pages/ChatRoomList.tsx
+++ APP(Android)/meerkat_fe/pages/ChatRoomList.tsx
@@ -1,17 +1,24 @@
-import {StyleSheet, Text, View } from "react-native"
-
+import { StyleSheet, Text, View } from "react-native";
 
 export default function ChatRoomList() {
-    
-    return <View style={styles.container}><Text>hi</Text></View>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>대화방</Text>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    paddingTop: 80,
+    paddingLeft: 10,
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+  title: {
+    fontSize: 40,
+    fontFamily: "noto-bold",
+  },
+});
