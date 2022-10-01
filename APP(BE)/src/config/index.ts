2c5c311a5d2768cce2c1e1278611aa2a6bf2d588
root <root@instance-4.asia-northeast3-a.c.vscode-353910.internal>
2022-10-01 08:32:22 +0000

    MK-47 fix: fix conflict 2

--- APP(BE)/src/config/index.ts
+++ APP(BE)/src/config/index.ts
@@ -1,4 +1,5 @@
 import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
+export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT, HTTP_PORT, HTTPS_PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, KEY_URL, SSL_NAME } = process.env;
