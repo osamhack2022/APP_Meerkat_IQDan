50cb9ad39c3fdc6a8a14f480fc6e847f264785b7
hyelie <hyelie@postech.ac.kr>
2022-10-02 05:26:37 +0000

    MK-32 feat: Create me in friend list page, change font 친구 목록에 '나' 추가, header에 구분선 추가, 폰트 변경으로 인해 lineheight 추가

--- APP(Android)/meerkat_fe/components/FriendList/FriendBox.tsx
+++ APP(Android)/meerkat_fe/components/FriendList/FriendBox.tsx
@@ -1,105 +1,112 @@
-import { StyleSheet, View, Text, Image, ScrollView } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import { UserEvent, UserProfile } from "../../common/types.d";
 
-function getEventImage(event: UserEvent | null){
-    if(event == UserEvent.NONE || event == null){
-        return <></>
-    }
-
-    var eventImageSource;
-    if(event == UserEvent.RESERVE){
-        eventImageSource = require('../../assets/user_event/be_reserved.jpg');
-    }
-    else if(event == UserEvent.PROMOTION){
-        eventImageSource = require('../../assets/user_event/be_promotion.jpg');
-    }
-    return eventImageSource;
+/**
+ * @param event typeof enum UserEvent | null
+ * @returns the image layer according to the image type.
+ */
+function getEventImage(event: UserEvent | null | undefined) {
+  if (event === UserEvent.NONE || event === null || event === undefined) {
+    return <></>;
+  }
+  if (event === UserEvent.RESERVE) {
+    return (
+      <Image
+        style={styles.eventImage}
+        source={require("../../assets/users/reserving.jpg")}
+      />
+    );
+  } else if (event === UserEvent.PROMOTION) {
+    return (
+      <Image
+        style={styles.eventImage}
+        source={require("../../assets/users/promotion.jpg")}
+      />
+    );
+  }
 }
 
 export default function FriendBox(props: UserProfile) {
-  const { name, image, event, statusMessage } = props;
-     const ProfileImageSource = require('../../assets/user_event/empty_profile.jpg')
-//   const ProfileImageSource = image === null
-                                // ? require('../../assets/user_event/empty_profile.jpg')
-                                // : require(image);
-  const EventImageSource = getEventImage(event);
+  const { name, image, event, statusMessage } = props; // profile image must be delivered as prop
+
+  const ProfileImageSource =
+    image === null || image == undefined
+      ? require("../../assets/users/emptyProfile.jpg")
+      : image;
 
   return (
     <View style={styles.container}>
-        <Image style={styles.profileImage} source={ProfileImageSource} /> 
-        <View style={styles.mainContainer}>
-            <View style={styles.nameLayout}>
-                <Text style={styles.nameText}>
-                    {name}
-                </Text>
-                <Image style={styles.eventImage} source={EventImageSource} /> 
-            </View>
-            <Text style={styles.statusMessageText}>
-                {statusMessage}
-            </Text>
+      <Image style={styles.profileImage} source={ProfileImageSource} />
+      <View style={styles.nameContainer}>
+        <View style={styles.nameLayout}>
+          <Text style={styles.nameText}>{name}</Text>
+          {getEventImage(event)}
         </View>
-        <View style={styles.ddayContainer}>
-            <Text style={styles.ddayText}>
-                D-100
-            </Text>
-        </View>
-        
+        {statusMessage === "" ? (
+          <></>
+        ) : (
+          <Text style={styles.statusMessageText}>{statusMessage}</Text>
+        )}
+      </View>
+      <View style={styles.ddayContainer}>
+        <Text style={styles.ddayText}>D-100</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        height: 65,
-        alignItems: "center",
-        justifyContent: "flex-start",
-        flexDirection: "row",
-        backgroundColor: "white"
-    },
-    profileImage:{
-        width: 46,
-        height: 46,
-        borderRadius: 17,
-        marginLeft: 18,
-        marginRight: 12
-    },
-    mainContainer: {
-        flexDirection: "column"
-    },
-    nameLayout:{
-        flexDirection: "row",
-        alignItems: "center",
-    },
-    nameText:{
-        fontSize: 19,
-        fontFamily: "noto-reg",
-        lineHeight: 20
-    },
-    eventImage:{
-        marginLeft: 6,
-        width: 15,
-        height: 15
-    },
-    statusMessageText:{
-        marginTop: 3,
-        fontSize: 11,
-        color: "rgba(0, 0, 0, 0.45)"
-    },
-    ddayContainer:{
-        position: "absolute",
-        borderRadius: 4,
-        padding: 4,
-        right: 18,
-        backgroundColor: "#D6D6D6"
-    },
-    ddayText:{
-        fontSize: 12,
-        textAlign: "center",
-        textAlignVertical: "center"
-    }
-
-
-})
-
-
-
+  container: {
+    height: 65,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    flexDirection: "row",
+    backgroundColor: "white",
+  },
+  profileImage: {
+    width: 46,
+    height: 46,
+    borderRadius: 17,
+    marginLeft: 18,
+    marginRight: 12,
+  },
+  nameContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  nameLayout: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  nameText: {
+    fontSize: 18,
+    fontFamily: "noto-reg",
+    lineHeight: 25,
+  },
+  eventImage: {
+    marginLeft: 6,
+    width: 15,
+    height: 15,
+  },
+  statusMessageText: {
+    fontSize: 11,
+    color: "rgba(0, 0, 0, 0.45)",
+    fontFamily: "noto-reg",
+    lineHeight: 20,
+  },
+  ddayContainer: {
+    position: "absolute",
+    borderRadius: 4,
+    padding: 4,
+    right: 18,
+    backgroundColor: "#D6D6D6",
+  },
+  ddayText: {
+    flex: 1,
+    fontSize: 12,
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontFamily: "noto-reg",
+    lineHeight: 15
+  },
+});
---
+++ APP(Android)/meerkat_fe/components/FriendList/MyBox.tsx
@@ -0,0 +1,59 @@
+import { StyleSheet, View, Text, Image } from "react-native";
+import { UserProfile } from "../../common/types.d";
+
+export default function FriendBox(props: UserProfile) {
+  const { name, image, statusMessage } = props; // profile image must be delivered as prop
+
+  const ProfileImageSource = ((image === null || image === undefined)
+                                ? require('../../assets/users/emptyProfile.jpg')
+                                : image);
+
+  return (
+    <View style={styles.container}>
+        <Image style={styles.profileImage} source={ProfileImageSource}/>
+        <View style={styles.nameContainer}>
+            <Text style={styles.nameText}>
+                {name}
+            </Text>
+            <Text style={styles.statusMessageText}>
+                {statusMessage}
+            </Text>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        height: 91,
+        alignItems: "center",
+        justifyContent: "flex-start",
+        flexDirection: "row",
+        backgroundColor: "white"
+    },
+    profileImage:{
+        width: 65,
+        height: 65,
+        borderRadius: 19,
+        marginLeft: 18,
+        marginRight: 12
+    },
+    nameContainer: {
+        flexDirection: "column",
+    },
+    nameText:{
+        fontSize: 19,
+        fontFamily: "noto-reg",
+        lineHeight: 25,
+    },
+    statusMessageText:{
+        marginTop: 3,
+        fontSize: 11,
+        fontFamily: "noto-reg",
+        lineHeight: 20,
+        color: "rgba(0, 0, 0, 0.45)"
+    }
+})
+
+
+
