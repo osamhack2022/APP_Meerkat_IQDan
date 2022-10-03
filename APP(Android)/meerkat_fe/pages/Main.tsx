ff437f9fe61b5396b8b01a0e3666d199ab924fa6
Seho Lee <31482043+seho0808@users.noreply.github.com>
2022-10-03 07:00:41 +0000

    MK-57 feat: icons

--- APP(Android)/meerkat_fe/pages/Main.tsx
+++ APP(Android)/meerkat_fe/pages/Main.tsx
@@ -1,31 +1,61 @@
+// comps
 import Friend from "./FriendList";
 import ChatRoomList from "./ChatRoomList";
 import Settings from "./Settings";
-
+// nav
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { TabParamList } from "../common/types";
+// assets
+import { Ionicons, Entypo } from "@expo/vector-icons";
 
 export default function Main() {
     const Tab = createBottomTabNavigator<TabParamList>();
 
     return (
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={{tabBarStyle: {height: 50}}}>
             <Tab.Screen
                 name="Friends"
                 component={Friend}
-                options={{ headerShown: false }}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <Ionicons name="people" size={30} color={focused ? "#6A4035" : "#E5B47F"} />
+                    ),
+                    tabBarLabelStyle: {
+                        fontSize: 0
+                    }
+                }}
             />
             <Tab.Screen
                 name="ChatRoomList"
                 component={ChatRoomList}
-                options={{ headerShown: false }}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <Entypo name="chat" size={30} color={focused ? "#6A4035" : "#E5B47F"} />
+                    ),
+                    tabBarLabelStyle: {
+                        fontSize: 0
+                    }
+                }}
             />
             <Tab.Screen
                 name="Settings"
                 component={Settings}
-                options={{ headerShown: false }}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <Ionicons
+                            name="settings-sharp"
+                            size={30}
+                            color={focused ? "#6A4035" : "#E5B47F"}
+                        />
+                    ),
+                    tabBarLabelStyle: {
+                        fontSize: 0
+                    }
+                }}
             />
         </Tab.Navigator>
     );
 }
-``;
