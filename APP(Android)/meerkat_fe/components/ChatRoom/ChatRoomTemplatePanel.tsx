50e40f8a3ce19dcd2b7591dd12947a3abbf75545
exqt <exqt_@naver.com>
2022-10-07 10:36:02 +0000

    MK-89 feat: WIP implement storage & modification

--- APP(Android)/meerkat_fe/components/ChatRoom/ChatRoomTemplatePanel.tsx
+++ APP(Android)/meerkat_fe/components/ChatRoom/ChatRoomTemplatePanel.tsx
@@ -1,7 +1,10 @@
-import React, { useRef, useState } from "react";
+import AsyncStorage, { AsyncStorageStatic } from "@react-native-async-storage/async-storage";
+import React, { useEffect, useRef, useState } from "react";
 import { Alert, Modal, StyleSheet, Text, Pressable, View, KeyboardAvoidingView, SafeAreaView } from "react-native";
 import { MaterialIcons } from '@expo/vector-icons'
 import { ScrollView, TextInput, TouchableOpacity } from "react-native-gesture-handler";
+
+const TemplateStorageKey = "TemplateStorageKey"
 
 interface ChatRoomTemplateModalProps {
   visible: boolean
@@ -10,35 +13,42 @@ interface ChatRoomTemplateModalProps {
 }
 
 interface TemplateItem {
-  _id: number,
+  id: number,
   name: string,
   content: string
 }
 
-const templateList: TemplateItem[] = [
-  {_id: 1, name: "휴가 출발", content: "충성!\n상병 전형록 휴가 (0.0 ~ 0.0) 출발하여 YY에 도착하였습니다!"},
-  {_id: 2, name: "휴가 복귀", content: "충성!\n상병 전형록 휴가 (0.0 ~ 0.0) 복귀 후 지통실에 보고하였습니다!"},
-  {_id: 3, name: "인원 보고", content: "충성!\n9생활관 총원 9명 이상없습니다."},
-  {_id: 4, name: "이상무", content: "충성! 순찰 이상없습니다."},
-  {_id: 5, name: "이상무", content: "충성! 순찰 이상없습니다."},
-  {_id: 6, name: "이상무", content: "충성! 순찰 이상없습니다."},
-  {_id: 7, name: "이상무", content: "충성! 순찰 이상없습니다."},
-  {_id: 8, name: "이상무", content: "충성! 순찰 이상없습니다."},
-  {_id: 9, name: "이상무", content: "충성! 순찰 이상없습니다."},
+const defaultTemplateList: TemplateItem[] = [
+  {id: 1, name: "휴가 출발", content: "충성!\n상병 전형록 휴가 (0.0 ~ 0.0) 출발하여 YY에 도착하였습니다!"},
+  {id: 2, name: "휴가 복귀", content: "충성!\n상병 전형록 휴가 (0.0 ~ 0.0) 복귀 후 지통실에 보고하였습니다!"},
+  {id: 3, name: "인원 보고", content: "충성!\n9생활관 총원 9명 이상없습니다."},
 ]
 
-const TemplateItem = (props: {item: TemplateItem, onApply: () => void, scrollTo: (item: any) => void}) => {
+interface TemplateItemProps {
+  item: TemplateItem
+  onApply: () => void
+  scrollTo: (item: any) => void
+  updateItem: (id: number, name: string, content: string) => void
+}
+
+const TemplateItem = (props: TemplateItemProps) => {
+  const { item } = props;
   const [editVisible, setEditVisible] = useState(false)
   const nameInputRef = useRef<TextInput>(null);
   const contentInputRef = useRef<TextInput>(null);
+
+  const [name, setName] = useState(item.name);
+  const [content, setContent] = useState(item.content);
 
   return (
     <View style={styles.item}>
       <TextInput 
         ref={nameInputRef}
         style={styles.itemName}
-        value={props.item.name}
+        value={name}
         onPressIn={() => props.scrollTo(nameInputRef?.current)}
+        onChangeText={setName}
+        onBlur={() => props.updateItem(item.id, name, content)}
       />
       <View style={{flexDirection: "row", justifyContent: "flex-end"  }}>
         <TouchableOpacity onPress={() => setEditVisible(!editVisible)}>
@@ -51,9 +61,11 @@ const TemplateItem = (props: {item: TemplateItem, onApply: () => void, scrollTo:
       {editVisible && <TextInput 
         ref={contentInputRef}
         style={{backgroundColor: "#EEE", padding: 8, borderRadius: 8}}
-        value={props.item.content}
+        value={content}
         multiline
         onPressIn={() => props.scrollTo(contentInputRef?.current)}
+        onChangeText={setContent}
+        onBlur={() => props.updateItem(item.id, name, content)}
       />}
     </View>
   )
@@ -61,6 +73,7 @@ const TemplateItem = (props: {item: TemplateItem, onApply: () => void, scrollTo:
 
 const ChatRoomTemplatePanel = (props: ChatRoomTemplateModalProps) => {
   let scrollRef = useRef<ScrollView>(null)  
+  let [templates, settempaltes] = useState<TemplateItem[]>([]);
   
   const scrollToItem = (item: TextInput) => {
     let responder = scrollRef.current?.getScrollResponder()
@@ -68,6 +81,29 @@ const ChatRoomTemplatePanel = (props: ChatRoomTemplateModalProps) => {
       responder?.scrollResponderScrollNativeHandleToKeyboard(item, 200, true)
     }, 150)
   }
+
+  const updateItem = (id: number, name: string, content: string) => {
+    let t = templates.filter((item) => item.id != id);
+    t.push({id, name, content});
+    t = t.sort((x, y) => x.id - y.id);
+    settempaltes(t);
+    (async () => {
+      AsyncStorage.setItem(TemplateStorageKey, JSON.stringify(t));
+    })();
+  }
+
+  useEffect(() => {
+    let getData = async () => {
+      let res = await AsyncStorage.getItem(TemplateStorageKey)
+      if (res) {
+        settempaltes(JSON.parse(res));
+      } 
+      else {
+        settempaltes(defaultTemplateList); 
+      }
+    }
+    getData();
+  }, []);
 
   if (!props.visible) return null;
 
@@ -81,12 +117,13 @@ const ChatRoomTemplatePanel = (props: ChatRoomTemplateModalProps) => {
             </Pressable>
             <View style={styles.itemList}>
               {
-                templateList.map((item) =>
+                templates.map((item) =>
                   <TemplateItem
                     onApply={() => { props.setMsgInput(item.content); props.setVisible(false) }}
                     scrollTo={scrollToItem}
                     item={item}
-                    key={item._id}
+                    key={item.id}
+                    updateItem={updateItem}  
                   />)
               }
             </View >
