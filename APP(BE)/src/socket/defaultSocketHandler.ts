018dd5cfb34a88fc7d6c19119ec1915cb390045f
hyelie <hyelie@postech.ac.kr>
2022-10-10 15:56:26 +0000

    MK-65 etc: Change socket architecture socket.ts에서 모든 것을 작성하는 것이 아니라, middleware, global socket, room socket을 파일로 분할해서 가독성을 높힘

---
+++ APP(BE)/src/socket/defaultSocketHandler.ts
@@ -0,0 +1,20 @@
+import { Namespace, Socket } from "socket.io"
+
+// default socket event handler
+export default (io: Namespace, socket: Socket) =>{
+    socket.on("error", (err) =>{
+        // TODO : console log는 디버깅용, 추후 완성되면 삭제
+        console.log("오류 발생" + err);
+        socket.disconnect();
+    });
+
+    socket.on("connect_error", (err) =>{
+        // TODO : console log는 디버깅용, 추후 완성되면 삭제
+        console.log("connection 오류 발생" + err);
+    });
+
+    socket.on("disconnect", () =>{
+        // TODO : console log는 디버깅용, 추후 완성되면 삭제
+        console.log("a user disconnected");
+    });
+};
---
+++ APP(BE)/src/socket/socketio.ts
@@ -0,0 +1,55 @@
+import http from "http";
+import https from "https";
+import { Server, Socket } from "socket.io"
+import MessageSerivce from "@services/message.service";
+import globalSocketHandler from "./globalSocketHandler";
+import defaultSocketHandler from "./defaultSocketHandler";
+import roomSocketHandler from "./roomSocketHandler";
+import socketMiddleware from "./socketMiddleware";
+
+class SocketIO{
+    private ioServer: Server;
+    private messageService: MessageSerivce = new MessageSerivce();
+
+    constructor(server: http.Server | https.Server){
+        this.ioServer = new Server(server, {
+            cors: {
+                origin: "*",
+                credentials: true
+            },
+            path: "/socket.io",
+            transports: ["websocket", "polling"],
+        });
+
+        const chatio = this.ioServer.of("/chat");
+        chatio.on("connection", (socket:Socket)=>{
+            // TODO : debug용 console log
+            console.log("chat namespace new user!!");
+            console.log(socket.handshake.auth);
+
+            // token validator middleware. if unauthorized, then disconnect.
+            socketMiddleware(chatio, socket);
+            console.log(socket.handshake.auth.userId); // userId value가 나옴.
+            
+            // default socket
+            defaultSocketHandler(chatio, socket);
+            // global socket
+            globalSocketHandler(chatio, socket);
+            // room socket
+            roomSocketHandler(chatio, socket, this.messageService);
+        });
+
+
+        // 다른 connection일 경우 쳐냄.
+        this.ioServer.on("connection", (socket: Socket) => {
+            // TODO : debug용 console log
+            console.log("disconnect default connection");
+            socket.disconnect();
+        });
+    }
+}
+
+export default SocketIO;
+
+// socket.broadcast.to(roomid).emit -> socket caller빼고 room에 있는 사람에게 전달
+// this.ioServer.in(roomdid).emit -> room에 있는 모든 사람에게 전달
