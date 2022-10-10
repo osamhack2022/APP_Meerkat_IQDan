9e81ae0dc683e42f02e45183a3493ee0f9cbbbc2
Dongjin Lim <42141895+djlim98@users.noreply.github.com>
2022-10-10 08:10:38 +0000

    MK-140 feat: create settingLoadingPage

--- APP(Android)/meerkat_fe/pages/SettingsLoading.tsx
+++ APP(Android)/meerkat_fe/pages/SettingsLoading.tsx
@@ -1,6 +1,6 @@
 // core
-import { useContext, useEffect, useState } from "react";
-import { StyleSheet, View, TouchableOpacity,Text, Alert, Image } from "react-native";
+import { useContext, useEffect, useRef, useState } from "react";
+import { StyleSheet, View, TouchableOpacity,Text, Alert, Image,Animated } from "react-native";
 import {
     MaterialCommunityIcons,
     Feather,
@@ -9,131 +9,25 @@ import {
 } from "@expo/vector-icons";
 import * as reactNativePaper from 'react-native-paper';
 
+import getGlitterStyle from "../components/FriendList/getGlitterStyle";
+
 // thirds
 import AsyncStorage from "@react-native-async-storage/async-storage";
 // types
-import { MainTabScreenProps, User } from "../common/types";
+import { MainTabScreenProps, AnimatedValue, User } from "../common/types";
 // context
 import { LoginContext } from "../common/Context";
 import api from "../common/api";
 
+
 import getProfileSource from "../components/FriendList/getProfileSource";
-import SettingsLoading from '../pages/SettingsLoading';
 import { useIsFocused } from "@react-navigation/native";
 
 const FriendListKey = "FriendList";
 
-export default function Settings(props: MainTabScreenProps<"Settings">) {
-    const {navigation} = props;
-    const { checkIfLoggedIn } = useContext(LoginContext);
-    const [user, setUser] = useState<User|null>(null);
-    const [dDay, setDDday] = useState(0);
-    const [pageState, setPageState] = useState<string>("loading");
-    const [friends, setFriends] = useState<User[]>([]);
-    const isFocused = useIsFocused();
+export default function SettingsLoading() {
+  const animatedValue = useRef(new Animated.Value(0.4)).current;
 
-    useEffect(() => {
-      // load chat room data from async storage / also check for updates? no. data is updated via websocket or polling.
-      if(isFocused){
-      (async () => {
-        await fetchFromAsyncStorage();
-        // await sleep(2);
-        await fetchFriends();
-        await fetchMe(); 
-        await sleep(0.7);
-        setPageState("loaded");
-      })();
-      }
-         }, [isFocused]);
-
-  const fetchMe = async () => {
-      api
-        .get("/users/me")
-        .then((res) => {
-          let data = res.data.data as User;
-          setUser(data);
-          setDDday(computedDday(data.enlistmentDate));
-
-          
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-    }
-    const fetchFromAsyncStorage = async () => {
-      let friends = JSON.parse(await AsyncStorage.getItem(FriendListKey) || "[]")
-      
-      setFriends(friends);
-    };
-
-    const fetchFriends = async () => {
-      // axios get 후에 async storage set
-      // async storge에 넣고, async에서 받아오고, 그걸 리턴하면 ?
-  
-      let userToken = await AsyncStorage.getItem("userToken")
-  
-      api.get("/friends", {
-          headers: {
-            Authorization: "Bearer " + userToken,
-          },
-        })
-        .then(async (res) => {
-          let friends = res.data.data;
-          setFriends(friends);
-          AsyncStorage.setItem(FriendListKey, JSON.stringify(friends));
-        })
-        .catch((err) => {
-          // TODO : show error
-          setPageState("error");
-          console.log(err.response);
-        })
-    };
-    
-  const sleep = (time: number) => {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve("slept well"), time*1000)
-    });
-};
-  const computedDday=(enlistmentDate:any)=>{
-  const currentDate= new Date();
-  const enlistmentDateToDate= new Date(enlistmentDate);
-
-  const diffDate = currentDate.getTime() - enlistmentDateToDate.getTime();
-  return Math.floor(Math.abs(diffDate / (1000 * 60 * 60 * 24))); 
-  };
-
-    const handleMyProfile = () => {
-        navigation.navigate("MyProfile")
-    }
-
-    const handleChangePw = () => {
-        navigation.navigate("ChangePw")
-    }
-
-
-    const handleRemoveUser = () => {
-        Alert.alert(
-            ":/",
-            "현재 지원되지 않는 기능입니다.",
-            [
-                {
-                    text: "확인",
-                    onPress: () => {},
-                },
-            ]
-        );
-    }
-
-
-    const handleLogout = async () => {
-        await AsyncStorage.setItem("userToken", "");
-        await AsyncStorage.setItem("userTokenExpiration", "");
-        checkIfLoggedIn();
-        navigation.navigate("Auth")
-    };
-
-    if (pageState == "loading") {return <SettingsLoading />}
-    else if (pageState == "error") {return <View><Text>Error!!!!</Text></View>}
     return (
         <View style={styles.container}>
             <View style={styles.titleContainer}>
@@ -141,16 +35,17 @@ export default function Settings(props: MainTabScreenProps<"Settings">) {
             </View>
             <View style={styles.userInfoSection}>
         <View style={{flexDirection: 'row', marginTop: 15}}>
-          <Image style={styles.profileImage}
-            source={getProfileSource(user?.image)}
-          />
+        <View style={styles.profileImage}>
+                <Animated.View style={getGlitterStyle(animatedValue)} />
+            </View>
          
           <View style={{marginLeft: 20}}>
-            <reactNativePaper.Title style={[styles.title, {
-              marginTop:15,
-              marginBottom: 5,
-            }]}>{user?.name}</reactNativePaper.Title>
-            <reactNativePaper.Caption style={styles.caption}>{user?.militaryRank}</reactNativePaper.Caption>
+          <View style={styles.titleText}>
+                    <Animated.View style={getGlitterStyle(animatedValue)} />
+                </View>
+                <View style={styles.nameText}>
+                    <Animated.View style={getGlitterStyle(animatedValue)} />
+                </View>
 
           </View>
         </View>
@@ -160,18 +55,23 @@ export default function Settings(props: MainTabScreenProps<"Settings">) {
         <View style={styles.userInfo}>
           <Feather name="home" color="#6A4035" size={20}/>
           <Text style={{color:"black", marginLeft: 20,fontFamily: "noto-bold",}}>소속부대</Text>
-          <Text style={{color:"#6A4035", marginLeft: 20}}>{user?.affiliatedUnit}</Text>
+          <View style={styles.nameText}>
+                    <Animated.View style={getGlitterStyle(animatedValue)} />
+                </View>
         </View>
         <View style={styles.userInfo}>
           <MaterialIcons  name="confirmation-number" color="#6A4035" size={20}/>
           <Text style={{color:"black", marginLeft: 20,fontFamily: "noto-bold",}}>군번        </Text>
-          <Text style={{color:"#6A4035", marginLeft: 20}}>{user?.serviceNumber}</Text>
+          <View style={styles.nameText}>
+                    <Animated.View style={getGlitterStyle(animatedValue)} />
+                </View>
         </View>
         <View style={styles.userInfo}>
         <AntDesign name="idcard" size={20} color="#6A4035" />
         <Text style={{color:"black", marginLeft: 20,fontFamily: "noto-bold",}}>아이디    </Text>
-          <Text style={{color:"#6A4035", marginLeft: 20}}>{user?.uid}</Text>
-
+        <View style={styles.nameText}>
+                    <Animated.View style={getGlitterStyle(animatedValue)} />
+                </View>
         </View>
       </View>
 
@@ -181,18 +81,18 @@ export default function Settings(props: MainTabScreenProps<"Settings">) {
             borderRightWidth: 1
           }]}>
 
-            <reactNativePaper.Title style={styles.infoBoxText}>D-{dDay}</reactNativePaper.Title>
+            <reactNativePaper.Title style={styles.infoBoxText}><Animated.View style={getGlitterStyle(animatedValue)} /></reactNativePaper.Title>
             <reactNativePaper.Caption style={styles.caption}>전역까지</reactNativePaper.Caption>
           </View>
           <View style={styles.infoBox}>
-            <reactNativePaper.Title style={styles.infoBoxText}>{friends.length}</reactNativePaper.Title>
+            <reactNativePaper.Title style={styles.infoBoxText}><Animated.View style={getGlitterStyle(animatedValue)} /></reactNativePaper.Title>
             <reactNativePaper.Caption style={styles.caption}>친구</reactNativePaper.Caption>
 
           </View>
       </View>
 
       <View style={styles.menuWrapper}>
-        <reactNativePaper.TouchableRipple onPress={handleChangePw}>
+        <reactNativePaper.TouchableRipple onPress={()=>{}}>
           <View style={styles.menuItem}>
 
           <MaterialCommunityIcons name="key-change" size={24} color="#6A4035" />
@@ -200,7 +100,7 @@ export default function Settings(props: MainTabScreenProps<"Settings">) {
 
           </View>
         </reactNativePaper.TouchableRipple>
-        <reactNativePaper.TouchableRipple onPress={handleMyProfile}>
+        <reactNativePaper.TouchableRipple onPress={()=>{}}>
           <View style={styles.menuItem}>
 
           <AntDesign name="profile" size={24} color="#6A4035" />
@@ -208,7 +108,7 @@ export default function Settings(props: MainTabScreenProps<"Settings">) {
 
           </View>
         </reactNativePaper.TouchableRipple>
-        <reactNativePaper.TouchableRipple onPress={handleLogout}>
+        <reactNativePaper.TouchableRipple onPress={()=>{}}>
           <View style={styles.menuItem}>
 
           <MaterialIcons name="logout" size={24} color="#6A4035" />
@@ -317,9 +217,26 @@ const styles = StyleSheet.create({
       width: 80,
       height: 80,
       borderRadius: 17,
+      backgroundColor: "#DBDBDB",
     },
     userInfo:{
       flexDirection: 'row',
       alignItems: 'center'
-    }
+    },
+    nameText: {
+      lineHeight: 25,
+      height: 19,
+      backgroundColor: "#DBDBDB",
+      width: 45,
+      marginLeft: 20
+  },
+  titleText: {
+    lineHeight: 25,
+    height: 30,
+    backgroundColor: "#DBDBDB",
+    width: 80,
+    marginLeft: 20,
+    marginTop:15,
+    marginBottom: 5,
+},
 });
