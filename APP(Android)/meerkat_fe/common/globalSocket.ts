3d4a1c292c980a0ac67c46cdb622056b8d2163f9
hyelie <hyelie@postech.ac.kr>
2022-10-13 13:30:46 +0000

    MK-65 fix: unread messages query & remove useless console log

--- APP(Android)/meerkat_fe/common/globalSocket.ts
+++ APP(Android)/meerkat_fe/common/globalSocket.ts
@@ -12,15 +12,9 @@ export const globalSocketFunction = (socket: Socket) => {
     socket.on('connect', () => {
       // TODO : console log는 socket 디버깅용, 추후 완성되면 삭제
       console.log('--------------- global socket ---------------');
-
-      socket.on("server:joinedChatroom", (log:string)=>{
-        console.log(log);
-      })
       
       // 재접속 시 DB에서 속해있는 모든 방의 정보를 가져온 후, 그 방에 전부 접속해야 함.
       connectBelongRooms(socket);
-
-      
     });
   
     socket.on("disconnect", () => {
@@ -52,15 +46,6 @@ export const globalSocketFunction = (socket: Socket) => {
     socket.io.on('reconnect_failed', () => {
       console.log('socket reconnect faile ');
     });
-  
-    // TODO : client 단에서는 volatile 쓰면 안됨. + if(socket.connected)일 때 socket.emit() 하는 것으로.
-    // ex)
-    // let count = 0;
-    // setInterval(() => {
-    //     if(socket.connected){
-    //         socket.timeout(5000).emit('testsend', ++count);
-    //     }
-    // }, 2000);
   }
   
 
@@ -69,7 +54,6 @@ export const globalSocketFunction = (socket: Socket) => {
     .then((res) => {
       const chatrooms:Chatroom[] = res.data.data;
       const chatroomIds:number[] = chatrooms.map(chatroom=>chatroom.chatroomId);
-      console.log(chatroomIds);
       socket.emit("client:joinAllChatrooms", chatroomIds);
     })
     .catch((err)=>{
